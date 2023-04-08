fefd5451a15a0e52ff56bf8289f63302f0c08f5a
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2023-04-08 12:19:33 +0200

    setup

---
+++ models/User.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+});
+
+module.exports = model("User", userSchema);
